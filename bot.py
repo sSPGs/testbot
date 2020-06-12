00e006e38ced57bea3cf87a6fcc76b695262efc4
Vladimir Hrechuha <xtspgtx@gmail.com>
2020-06-12 22:06:11 +0300

    first commit

--- bot.py
+++ bot.py
@@ -3,6 +3,6 @@ import telebot
 bot = telebot.Telebot('1079190137:AAFcTWcmKiTPEmAUowbL1pdN3jw1YzSUrFY')
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+    bot.send_message(message.chat.id, 'Привет, ты yjjjfcсал мне /start')
 
 bot.polling()
